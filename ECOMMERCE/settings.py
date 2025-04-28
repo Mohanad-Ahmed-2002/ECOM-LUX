40c187419016d694d9151d78747977aa85cd649b
Mohanad-Ahmed-2002 <mohanadahmed092>
2025-04-28 05:20:36 +0300

    edit ssl

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -96,6 +96,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
 }}
 
 
