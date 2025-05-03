45cac0895d3ed91e6ee7fd0ae6ae2dc20d4cbe94
Mohanad-Ahmed-2002 <mohanadahmed092>
2025-05-04 02:47:28 +0300

    debug

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.luxflex2020.com', 'luxflex2020.com', '127.0.0.1', 'localhost']
 
