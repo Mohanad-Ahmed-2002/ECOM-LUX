1a11d129446923a8a3f9b67832c77a48bc330c42
Mohanad-Ahmed-2002 <mohanadahmed092>
2025-05-04 02:19:53 +0300

    edit

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -179,28 +179,3 @@ CACHES = {
 }
 
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
-# ✅ Force HTTPS redirect on Render
-SECURE_SSL_REDIRECT = True
-
-# ✅ Support "X-Forwarded-Proto" header from Render
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# ✅ Logging to console (especially for Render logs)
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-}
