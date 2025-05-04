4b688426264adebfc482c4cb00fe581bb7dbeb6d
Mohanad-Ahmed-2002 <mohanadahmed092>
2025-05-04 19:11:55 +0300

    fixed model

--- store/models.py
+++ store/models.py
@@ -86,6 +86,9 @@ class Government(models.Model):
         return self.name
 
 class CustomerOrder(models.Model):
+
+    STATUS_CHOICES = STATUS_CHOICES
+
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     address = models.CharField(max_length=255)
