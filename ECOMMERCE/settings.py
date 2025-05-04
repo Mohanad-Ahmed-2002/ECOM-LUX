4b3362b884c391ad957be56805781cf39af2ed83
Mohanad-Ahmed-2002 <mohanadahmed092>
2025-05-04 03:21:30 +0300

    test

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -17,6 +17,12 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+cloudinary.config(
+    cloud_name=config('CLOUD_NAME'),
+    api_key=config('API_KEY'),
+    api_secret=config('API_SECRET')
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
